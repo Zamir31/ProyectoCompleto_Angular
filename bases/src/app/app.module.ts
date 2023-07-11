d3b885338e93aa4e5eaddfe826f1be2685bf1f41
Daniel <noriegad258@gmail.com>
2023-07-11 15:49:53 -0600

    data binding y directivas

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero/hero.component';
+import { HeroesListaComponent } from './heroes/hero/lista/heroes-lista/heroes-lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    HeroComponent,
+    HeroesListaComponent
   ],
   imports: [
     BrowserModule
---
+++ bases/src/app/heroes/hero/lista/heroes-lista/heroes-lista.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-lista',
+  templateUrl: './heroes-lista.component.html',
+  styleUrls: ['./heroes-lista.component.css']
+})
+export class HeroesListaComponent {
+
+}
